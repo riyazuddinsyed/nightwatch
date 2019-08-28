0db83674541fae0e850748be77ef0b7c79a04827
riyazuddinsyed <49958950+riyazuddinsyed@users.noreply.github.com>
2019-08-28 15:02:09 -0700

    Update test.js

--- e2e/specs/test.js
+++ e2e/specs/test.js
@@ -1,48 +1,45 @@
-
 module.exports={
-    '@tags': ['signin'],
+    '@tags': ['cod'],
     step1:function (browser){
         browser
-        .url('https://coops-dev.pathfinder.gov.bc.ca/auth/')
+        .url('https://coops-test.pathfinder.gov.bc.ca/auth/')
         .waitForElementVisible('body',1000)
-        .setValue('#app > div.application--wrap > div.app-body > div > div > article > div > div > div > form > div:nth-child(2) > div > div > div.v-input__slot > div > input[type=text]','CP0001430')
-        .setValue('#app > div.application--wrap > div.app-body > div > div > article > div > div > div > form > div:nth-child(3) > div > div > div.v-input__slot > div.v-text-field__slot > input[type=password]','524590148')
-        browser.click('#app > div.application--wrap > div.app-body > div > div > article > div > div > div > form > div.passcode-form__row.passcode-form__form-btns > button.sign-in-btn.v-btn.v-btn--large.theme--light.primary > div')
+        .setValue('#app > div.application--wrap > div.app-body > div > div > article > div > div > div > form > div:nth-child(2) > div > div > div.v-input__slot > div > input[type=text]','CP0001229')
+        .setValue('#app > div.application--wrap > div.app-body > div > div > article > div > div > div > form > div:nth-child(3) > div > div > div.v-input__slot > div.v-text-field__slot > input[type=password]','391046331')
+        .click('#app > div.application--wrap > div.app-body > div > div > article > div > div > div > form > div.passcode-form__row.passcode-form__form-btns > button.sign-in-btn.v-btn.v-btn--large.theme--light.primary > div')
         .pause(10000)
     },
     step2:function(browser){
         browser
-        .click('#btn-standalone-addresses > div > span').pause(10000)
-        .click('#reg-off-addr-change-btn > div > span')
-        .waitForElementVisible('body',1000)
-
-        .click('[name=street-address]')
-        .clearValue('[name=street-address]')
-        .setValue('[name=street-address]','123test')
-
-        .click('[name=street-address-additional]')
-        .clearValue('[name=street-address-additional]')
-        .setValue('[name=street-address-additional]','321 test')
-         
-        .click('[name=address-city]')
-        .clearValue('[name=address-city]')
+        .click('#btn-standalone-directors').pause(10000)
+        .click('#directors > div:nth-child(2) > button > div')
+        .setValue('#new-director__first-name','test')
+        .setValue('input[aria-label="Initial"]','test2')
+        .setValue('input[aria-label="Last Name"]','test3')
+        .setValue('[name=street-address]','123 test st')
+        .setValue('[name=street-address-additional]','321 test st')
         .setValue('[name=address-city]','victoria')
-
-        .clearValue('[name=postal-code]')
-        .setValue('[name=postal-code]','V1V1V1')
-
-        .clearValue('[name=address-country]')
+        .click('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.meta-container__inner > form > div.form__row.three-column > div.v-input.item.v-text-field.v-text-field--box.v-text-field--enclosed.v-select.theme--light > div > div.v-input__slot > div.v-select__slot > div.v-select__selections')
+        .click('#app > div.v-menu__content.theme--light.menuable__content__active > div > div > div:nth-child(1) > a > div > div')
+        .setValue('[name=postal-code]','v1v1v1')
         .setValue('[name=address-country]','canada')
-         
-        .click('[name=delivery-instructions]')
-        .clearValue('[name=delivery-instructions]')
         .setValue('[name=delivery-instructions]','optional')
-
-        .click('#standalone-office-address-article > div > section:nth-child(1) > div > ul > li:nth-child(2) > div > div > div:nth-child(1) > div > div > div.v-input__slot > div > div')
-         
-        .click('#reg-off-update-addr-btn > div')
+        .click('#directors > div.v-card.v-card--flat.v-sheet.theme--light > ul.list.new-director > li > div > div > form > div.form__row.form__btns > button.form-primary-btn.v-btn.theme--light.primary > div')
+    },
+    step3:function(browser){
+        browser
+        .click('#director-1-cease-btn > div > span')
         .setValue('#certified-by-textfield','test')
         .click('#AR-step-4-container > div > div.v-input.v-input--selection-controls.v-input--checkbox.theme--light > div > div.v-input__slot > div > div')
-        .click('#coa-file-pay-btn')
+        .click('#cod-file-pay-btn')
+        .pause(10000)
+    },
+    step4:function(browser){
+        browser
+        .click('#paylistbutton > span.hidden-xs')
+        .click('#credit_payBtn')
+        .setValue('[name=trnCardNumber]','4030000010001234').pause(1000)
+        .setValue('[name=trnCardCvd]','123')
+        .click('#frmPayment > table:nth-child(10) > tbody > tr:nth-child(13) > td > table > tbody > tr > td:nth-child(3)')
     }
-};    
+};
